c1ade7e35464b2e5920208a90c0c0e2f4bb3889b
ViniciusLippel <viniciusmlippel@gmail.com>
2021-11-17 07:43:36 -0300

    KmerGenerator serial

--- src/k_mer_generator/KmerGenerator.java
+++ src/k_mer_generator/KmerGenerator.java
@@ -7,30 +7,43 @@ import java.util.concurrent.Executors;
 public class KmerGenerator{
 
 	private String dna;
+	private int k;
 	private ArrayList<String> k_mers = new ArrayList<String>();
 	private int numThreads = 4;
 	
-	public KmerGenerator(String dna) {
+	public KmerGenerator(String dna, int k) {
 		this.dna = dna;
+		this.k = k;
 	}
 	
+	//A geração de k-mers já está dividida pelo número de Threads
 	public void start() {
-		ExecutorService executorService = Executors.newFixedThreadPool(this.numThreads);
 		
 		int indexDif = this.dna.length()/numThreads;
+		System.out.println(indexDif);
 		
 		for (int i=0; i<numThreads; i++) {
 			int begin = indexDif*i;
 			int end = indexDif*(i+1);
 			
+			System.out.println(i);
 			if(i != numThreads-1) {
-				generate()
+				generate(begin, end);
+			}
+			else {
+				generate(begin, dna.length()-(k-1));
 			}
 		}
+		
+		System.out.println(k_mers.toString());
 	}
 	
-	public Runnable generate(int begin, int end) {
-		
+	//Gera os k-mers
+	public void generate(int begin, int end) {
+		for(int i=begin; i<end; i++) {
+			System.out.println(dna.substring(i, i+(k)));
+			this.k_mers.add(dna.substring(i, i+(k)));
+		}
 	}
 	
 }
--- src/k_mer_generator/Main.java
+++ src/k_mer_generator/Main.java
@@ -3,6 +3,8 @@ package k_mer_generator;
 public class Main {
 
 	public static void main (String[] args) {
+		KmerGenerator kmer = new KmerGenerator("AAGATTGGTATTA", 3);
 		
+		kmer.start();
 	}
 }
