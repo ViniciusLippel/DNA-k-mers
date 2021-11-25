a0a3748bec416f9c83b2d5eaa73a9745267d9561
ViniciusLippel <viniciusmlippel@gmail.com>
2021-11-25 14:07:03 -0300

    Update Main.java  and KmerGenerator.java

--- src/k_mer_generator/KmerGenerator.java
+++ src/k_mer_generator/KmerGenerator.java
@@ -1,24 +1,42 @@
 package k_mer_generator;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class KmerGenerator{
 
 	private String dna;
 	private int k;
 	private ArrayList<String> k_mers = new ArrayList<String>();
-	private int numThreads = 4;
+	private final int numThreads = 4;
+	private ExecutorService executorService;
 	
 	public KmerGenerator(String dna, int k) {
 		this.dna = dna;
 		this.k = k;
 	}
 	
-	//A geração de k-mers já está dividida pelo número de Threads
+	public ArrayList<String> getK_mers() {
+		return k_mers;
+	}
+	
+	public void setK_mers(ArrayList<String> k_mers) {
+		this.k_mers = k_mers;
+	}
+	
+	
+
+	public ExecutorService getExecutorService() {
+		return executorService;
+	}
+
 	public void start() {
 		
+		executorService = Executors.newFixedThreadPool(numThreads);
+		
 		int indexDif = this.dna.length()/numThreads;
-		System.out.println(indexDif);
 		
 		for (int i=0; i<numThreads; i++) {
 			int begin = indexDif*i;
@@ -27,21 +45,42 @@ public class KmerGenerator{
 			//System.out.println(i);
 			
 			if(i != numThreads-1) {
-				generate(begin, end);
+				executorService.execute(generate(begin, end));
 			}
 			else {
-				generate(begin, dna.length()-(k-1));
+				executorService.execute(generate(begin, dna.length()-(k-1)));
 			}
 		}
+		executorService.shutdown();
 		
-		System.out.println(k_mers.toString());
+		try {
+			executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+		} catch (InterruptedException e) {
+			System.out.println(k_mers.toString());
+		}
+		
 	}
 	
 	//Gera os k-mers
-	public void generate(int begin, int end) {
-		for(int i=begin; i<end; i++) {
-			//System.out.println(dna.substring(i, i+(k)));
-			this.k_mers.add(dna.substring(i, i+(k)));
+	public Runnable generate(int begin, int end) {
+		return new Runnable() {
+			public void run() {
+				for(int i=begin; i<end; i++) {
+					//System.out.println(Thread.currentThread().getName() + " " + dna.substring(i, (i+k)));
+					try {
+						k_mers.add(dna.substring(i, (i+k)));
+					} catch(ArrayIndexOutOfBoundsException exception) {
+						System.out.println(i + ", " + (i+k));
+					}
+					
+				}
+			}
+		};
+	}
+	
+	public void printKmers() {
+		for(int i=0; i<k_mers.size(); i++) {
+			System.out.println((k_mers.get(i)));
 		}
 	}
 	
--- src/k_mer_generator/Main.java
+++ src/k_mer_generator/Main.java
@@ -1,10 +1,27 @@
 package k_mer_generator;
 
+import java.io.FileNotFoundException;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-	public static void main (String[] args) {
-		KmerGenerator kmer = new KmerGenerator("AAGATTGGTATTA", 3);
+	public static void main (String[] args) throws FileNotFoundException, InterruptedException {
 		
+		String dna = new FastaReader().read("src/k_mer_generator/test8.fa");
+		
+		KmerGenerator kmer = new KmerGenerator(dna, 5);
+		long startTime = System.nanoTime();
 		kmer.start();
+		//kmer.getExecutorService().awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+		long endTime = System.nanoTime();
+		//kmer.printKmers();
+		System.out.println("Multithread time: " + (endTime-startTime));
+		System.out.println("end");
+		
+		SerialKmerGenerator skmer = new SerialKmerGenerator(dna, 5);
+		startTime = System.nanoTime();
+		skmer.start();
+		endTime = System.nanoTime();
+		System.out.println("Serial time: " + (endTime-startTime));
 	}
 }
